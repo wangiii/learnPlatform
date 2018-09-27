b36d267413623b373efc38b84a3449d54baf7072
wangiii <jasonwangiii@outlook.com>
2018-09-27 20:05:39 +0800

    完成基本页面布局

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+    <div class="container">
+        <p class="pull-left">
+            由 JasonVV 设计和编码 </span>
+        </p>
+
+        <p class="pull-right"><a href="mailto:jasonwangiii@outlook.com">联系我们</a></p>
+    </div>
+</footer>
