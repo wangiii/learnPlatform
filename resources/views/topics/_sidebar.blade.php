a3a0ef35f0c6c0efd247cc8bc4410e83b007e817
wangiii <jasonwangiii@outlook.com>
2018-09-29 01:05:22 +0800

    话题页

---
+++ resources/views/topics/_sidebar.blade.php
@@ -0,0 +1,10 @@
+<div class="panel panel-default">
+    <div class="panel-body">
+        右边导航栏
+    </div>
+</div>
+<div class="panel panel-default">
+    <div class="panel-body">
+        右边导航栏3244343434
+    </div>
+</div>
