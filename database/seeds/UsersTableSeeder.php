62559e28deaa1ff1765637803a8e585a21aafb07
JasonVV <jasonwangiii@outlook.com>
2018-10-05 17:10:49 +0800

    Add manage contents permission and visit horizon dashboard permission 增加内容管理权限和可访问horizon仪表盘的权限

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -43,19 +43,21 @@ class UsersTableSeeder extends Seeder
         $user->email = 'jasonwangiii@outlook.com';
         $user->password = bcrypt('wangchang');
         $user->avatar = 'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/ZqM7iaP4CR.png?imageView2/1/w/200/h/200';
+        $user->save();
 
         // 初始化用户角色，将 1 号用户指派为『站长』
         $user->assignRole('Founder');
-        $user->save();
 
         $user = User::find(2);
         $user->name = 'angiii';
         $user->email = '1054749081@qq.com';
         $user->password = bcrypt('wangchang');
         $user->avatar = 'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/ZqM7iaP4CR.png?imageView2/1/w/200/h/200';
+        $user->save();
+
         // 将 2 号用户指派为『管理员』
         $user->assignRole('Maintainer');
-        $user->save();
+
 
     }
 }
