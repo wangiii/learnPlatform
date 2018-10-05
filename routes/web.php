5b24f495369c21828200e9bc1197226d7d5724ba
wangiii <jasonwangiii@outlook.com>
2018-10-05 23:26:37 +0800

    设置默认主页

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@
 |
 */
 
-Route::get('/', 'TopicsController@index');
+Route::get('/', 'TopicsController@index')->name('root');
 
 // Authentication Routes...
 Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
